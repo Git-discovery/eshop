d53998b1475932f48a283003a67c75820c3985f2
sunsp <e542622761@outlook.com>
2019-09-27 23:22:07 +0800

    user element

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home
     },
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,14 @@
 <template>
-<el-form :model="form">
+
+<el-form ref="form" :model="form" label-width="100px">
   <el-form-item label="用户名">
       <el-input v-model="username" placeholder="用户名"></el-input>
   </el-form-item>
-  <el-form-item label="密&nbsp;码">
+  <el-form-item label="密码">
 <el-input v-model="password" placeholder="密码"></el-input>
   </el-form-item>
 </el-form>
-</el-form>
+
 </template>
 
 <script>
